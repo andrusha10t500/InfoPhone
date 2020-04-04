2eb162e3b427eccdc915f0b8a90f1fe8fd4f8364
andrusha10t500 <andrusha10t500@gmail.com>
2020-04-04 19:05:42 +0300

    v.5 (add files)

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get ugrade -y
+
+if [ ! $(which adb) ]
+then
+  echo 'Устанавливается последняя версия ADB:'
+  cd 
+  wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+  unzip \platform-tools-latest-linux.zip
+  sudo cp platform-tools/adb /usr/bin/adb
+  sudo cp platform-tools/fastboot /usr/bin/fastboot
+  rm -r platform-tools
+  rm platform-tools-latest-linux.zip
+fi
+
+if [ ! $(which qmake)]
+then
+  echo 'Устанавливается qmake:'
+  sudo apt-get install qt5-qmake -y
+fi
+
+if [ ! `which make` ]
+then 
+  echo 'Устанавливается make:'
+  sudo apt-get install make -y
+fi
+
+if [ ! `which g++` ]
+then 
+  echo 'Устанавливается g++:'
+  sudo apt-get install g++ -y
+fi
+
+cd 
+git clone https://github.com/andrusha10t500/InfoPhone.git
+cd InfoPhone
+qmake
+make
+sudo cp ./InfoPhone /usr/bin/infophone 
+cd ..
+sudo rm -r ./InfoPhone
+
