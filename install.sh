eab730662d7c79ed2ad722db72ce98377aa05808
andrusha10t500 <andrusha10t500@gmail.com>
2020-04-27 18:08:20 +0300

    install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
   echo 'Устанавливается последняя версия ADB:'
   cd 
   wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-  unzip \platform-tools-latest-linux.zip
+  unzip platform-tools-latest-linux.zip
   sudo cp platform-tools/adb /usr/bin/adb
   sudo cp platform-tools/fastboot /usr/bin/fastboot
   rm -r platform-tools
@@ -17,7 +17,7 @@ fi
 if [ ! $(which qmake)]
 then
   echo 'Устанавливается qmake:'
-  sudo apt-get install qt5-qmake -y
+  sudo apt-get install qt5-qmake qt5-default qml-module-qtquick* qml-module-qtgraphicaleffects qtdeclarative5-dev -y
 fi
 
 if [ ! `which make` ]
